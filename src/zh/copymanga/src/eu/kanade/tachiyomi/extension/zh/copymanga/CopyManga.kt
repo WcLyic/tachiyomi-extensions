322821928d828579414337e9262e2e5d2dbd1013
WcLyic <saberhana@yandex.com>
2023-09-01 14:39:37 +0800

    fix

--- src/zh/copymanga/src/eu/kanade/tachiyomi/extension/zh/copymanga/CopyManga.kt
+++ src/zh/copymanga/src/eu/kanade/tachiyomi/extension/zh/copymanga/CopyManga.kt
@@ -233,13 +233,14 @@ class CopyManga : ConfigurableSource, HttpSource() {
     }
 
     override fun headersBuilder() = super.headersBuilder()
-        .set("User-Agent", "Dart/2.16(dart:io)")
+        .set("user-agent", "COPY/2.0.7")
         .set("source", "copyApp")
-        .set("version", "1.3.7")
-        .set("region", if (preferences.getBoolean(CHANGE_CDN_OVERSEAS, false)) "0" else "1")
         .set("webp", if (preferences.getBoolean(CHANGE_WEBP_OPTION, false)) "1" else "0")
-        .set("authorization", "Token")
+        .set("authorization", "Token ba197de5d929b82447afc3e2c73456ed221ee9e4")
         .set("platform", "3")
+        .set("referer", "com.copymanga.app-2.0.7")
+        .set("version", "2.0.7")
+        .set("region", if (preferences.getBoolean(CHANGE_CDN_OVERSEAS, false)) "0" else "1")
 
     // Unused, we can get image urls directly from the chapter page
     override fun imageUrlParse(response: Response) =
@@ -428,6 +429,6 @@ class CopyManga : ConfigurableSource, HttpSource() {
         private const val CHANGE_CDN_OVERSEAS = "changeCDN"
         private const val CHANGE_WEBP_OPTION = "changeWebp"
         private const val API_URL_PREF = "apiUrl"
-        private val APIURLS = arrayOf("api.copymanga.org", "api.copymanga.com", "api.copymanga.net", "api.copymanga.info")
+        private val APIURLS = arrayOf("api.copymanga.org", "api.mangacopy.com", "api.copymanga.net", "api.copymanga.info", "api.copymanga.tv")
     }
 }
